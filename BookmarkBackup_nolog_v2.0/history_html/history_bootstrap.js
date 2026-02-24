c98670fc32245614ef60ffb72955946f2c4a6cb3
kwenxu <xukw123123@gmail.com>
2026-02-24 15:15:01 +0800

    本地仓库更新

--- BookmarkBackup_nolog_v2.0/history_html/history_bootstrap.js
+++
@@ -1,8 +0,0 @@
-// Lightweight bootstrap to set early page title from ?t=, before heavy scripts
-(function(){
-  try {
-    const t = new URLSearchParams(location.search).get('t');
-    if (t) document.title = t;
-  } catch(_) {}
-})();
-
